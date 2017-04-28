c3e1af3320f1b32c7305177c24d4f3601b41ae73
Jay <qiu.jay892@gmail.com>
2017-04-28 17:46:57 +0800

    add screenshot

--- app/src/main/java/com/qiuajy/demo/MainActivity.java
+++ app/src/main/java/com/qiuajy/demo/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setAllowFileAccessFromFileURLs(true);
 
         webView.loadUrl("file:///android_asset/pdf-website/index.html?pdf=../pdf/packt-gradle-for-android.pdf");
-        
+
 
         // assets/pdf-website/ 就是阅读 pdf 的站点
         // 如果想加载网络资源 pdf，需要解决跨域问题，这个需要开发者自己去解决了，推荐加载本地 pdf，如果是网络的文件
